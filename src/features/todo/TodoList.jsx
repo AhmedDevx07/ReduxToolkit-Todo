e9ed443ac53afa1eff06e76c5e48ae22f3520ccb
Ahmed-Devx <ahmedoffice69@gmail.com>
2026-04-13 00:21:30 +0500

    TodoList for Show status Todos complete/pending and no todos

---
+++ src/features/todo/TodoList.jsx
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+import TodoItem from "../../components/TodoItem";
+
+export default function TodoList() {
+  const { todos, filter } = useSelector((state) => state.todo);
+
+  const filtered = todos.filter((todo) => {
+    if (filter === "completed") return todo.completed;
+    if (filter === "pending") return !todo.completed;
+    return true;
+  });
+
+  if (!filtered.length) return <p>No todos 😴</p>;
+
+  return filtered.map((todo) => <TodoItem key={todo.id} todo={todo} />);
+}
